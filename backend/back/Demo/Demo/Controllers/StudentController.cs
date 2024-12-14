95809a6dba66b706a6c766051a9a6eb57631d5ab
youssef <youssefdid8@gmail.com>
2024-12-14 20:57:40 +0200

    adding backend

---
+++ backend/back/Demo/Demo/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Demo.DTO;
+using Demo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly Mdatabase db;
+        public StudentController(Mdatabase dbase)
+        {
+            db = dbase;
+        }
+
+       
+    
+
+   }
+}
---
+++ backend/back/Demo/Demo/Models/Resources.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Demo.Models
+{
+    public class Resources
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public int Progress { get; set; }
+        public int Tudone { get; set; }
+        public string Link { get; set; }
+        [ForeignKey("Learner")]
+        public int Learnerid { get; set; }
+        [ForeignKey("Category")]
+        public int Categoryid { get; set; }
+        [JsonIgnore]
+        public virtual Learner Learner { get; set; }
+        [JsonIgnore]
+        public virtual Category Category { get; set; }
+        public int Percentage
+        {
+            get
+            {
+                return Tudone != 0 ? (Progress * 100) / Tudone : 0; 
+            }
+        }
+
+        public DateTime Createdat { get; set; }
+        public DateTime Updatedat { get; set; }
+
+    }
+}
