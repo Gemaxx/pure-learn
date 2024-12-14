0d82d95595ef0194fb576e72f30ab06345a9183d
Gemax <Gemax.hope@gmail.com>
2024-12-14 21:06:15 +0200

    Change Goal Model

--- backend/back/Demo/Demo/Models/Goals.cs
+++ backend/back/Demo/Demo/Models/Goals.cs
@@ -6,27 +6,19 @@ using System.Text.Json.Serialization;
 
 namespace Demo.Models
 {
-    public class Goals
+    public class Goal
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Motivation { get; set; }
-
         public string Status { get; set; }
-
-        public int Priority { get; set; }
-        [ForeignKey("Learner")]
-        public int Learnerid { get; set; }
-        [JsonIgnore]
-        public virtual Learner Learner { get; set; }
-        public DateTime Createdat { get; set; }
-        public DateTime Updatedat { get; set; }
-       
-
-
-
-
-
+        public DateTime? CompletionDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public long? CategoryID { get; set; }
+        public long LearnerID { get; set; }
+        public Learner Learner { get; set; }
+        public Category Category { get; set; }
     }
 }
