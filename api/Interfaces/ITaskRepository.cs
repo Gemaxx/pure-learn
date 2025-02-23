2ca6ba23079846abf5ef7361a34aa17745ce892e
salmakaram <salma.elsayed.karam@gmail.com>
2025-02-23 17:32:09 +0200

    Add Task management functionality with DTOs and repository interface

---
+++ api/Interfaces/ITaskRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Helpers;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<List<Models.Task>> GetTasksAsync(long learnerId, TaskQueryObjects query);
+        Task<Models.Task?> GetTaskAsync(long learnerId, long taskId);
+        Task<Models.Task> CreateTaskAsync(long learnerId, Models.Task task);
+        Task<Models.Task?> UpdateTaskAsync(long learnerId, long taskId, Models.Task task);
+        Task<bool> DeleteTaskAsync(long learnerId, long taskId);
+        Task<bool> SoftDeleteTaskAsync(long learnerId, long taskId);
+        Task<bool> RestoreTaskAsync(long learnerId, long taskId);
+    }
+}
