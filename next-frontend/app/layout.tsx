3de4586ae855e580959d02a232f71bb7726d041c
Amal605 <amaltarek631@gmail.com>
2025-03-15 19:53:56 +0200

    Update main layout styles: set width and height for better responsiveness

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         >
           <SidebarProvider>
             <AppSidebar />
-            <main>
+            <main className="w-full h-full">
               <SidebarTrigger />
               {children}
             </main>
