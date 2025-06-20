20e623f010443a735b6777ddcf3075fe393a4a36
salmakaram <salma.elsayed.karam@gmail.com>
2025-06-20 03:12:30 +0300

    fixed tasktype icon problem2

--- api/Program.cs
+++ api/Program.cs
@@ -64,7 +64,11 @@ var app = builder.Build();
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "PureLearn API v1");
+            options.RoutePrefix = string.Empty; // This makes Swagger UI available at the root URL
+        });
     }
 
     app.UseHttpsRedirection();
