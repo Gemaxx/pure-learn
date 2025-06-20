0938c97e4288511083330d3afa62e23de3fbf83d
alaa227 <alaakhalid227@gmail.com>
2025-06-20 08:31:41 +0300

    Revert "fixed tasktype icon problem2"
    This reverts commit 20e623f010443a735b6777ddcf3075fe393a4a36.

--- api/Mapper/TaskTypeMapper.cs
+++ api/Mapper/TaskTypeMapper.cs
@@ -24,48 +24,12 @@ namespace api.Mapper
         // Map from CreateTaskTypeDto to TaskType model.
         public static TaskType ToTaskTypeFromCreateDto(this CreateTaskTypeDto createDto)
         {
-            byte[]? iconBytes = null;
-            
-            if (!string.IsNullOrEmpty(createDto.Icon))
-            {
-                try
-                {
-                    // Check if it's a valid base64 string
-                    if (IsValidBase64String(createDto.Icon))
-                    {
-                        iconBytes = Convert.FromBase64String(createDto.Icon);
-                    }
-                }
-                catch
-                {
-                    // If conversion fails, set to null
-                    iconBytes = null;
-                }
-            }
-
             return new TaskType
             {
                 Name = createDto.Name,
                 Description = createDto.Description,
-                Icon = iconBytes
+                Icon = !string.IsNullOrEmpty(createDto.Icon) ? Convert.FromBase64String(createDto.Icon) : null
             };
-        }
-
-        // Helper method to validate base64 string
-        private static bool IsValidBase64String(string base64)
-        {
-            if (string.IsNullOrEmpty(base64))
-                return false;
-
-            try
-            {
-                Convert.FromBase64String(base64);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
         }
 
         // Update an existing TaskType using PatchTaskTypeDto.
@@ -76,21 +40,7 @@ namespace api.Mapper
             if (!string.IsNullOrEmpty(patchDto.Description))
                 taskType.Description = patchDto.Description;
             if (!string.IsNullOrEmpty(patchDto.Icon))
-            {
-                try
-                {
-                    // Check if it's a valid base64 string
-                    if (IsValidBase64String(patchDto.Icon))
-                    {
-                        taskType.Icon = Convert.FromBase64String(patchDto.Icon);
-                    }
-                }
-                catch
-                {
-                    // If conversion fails, keep existing icon or set to null
-                    taskType.Icon = null;
-                }
-            }
+                taskType.Icon = Convert.FromBase64String(patchDto.Icon);
         }
     }
 }
